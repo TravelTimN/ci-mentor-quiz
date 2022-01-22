ffd158f2e402a7783db556c8a6b7c102168c376c
Tim Nelson <traveltimn@gmail.com>
2022-01-22 17:12:42 +0000

    Update admin.py for accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import Profile
 
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "taken_quiz")  # table view
     list_filter = ("taken_quiz",)  # sidebar filter
     search_fields = ["user", "taken_quiz"]  # search box
