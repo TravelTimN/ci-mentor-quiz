b1c4d3229c4dc5fb79d4d9974c5cf74922d38ae6
Tim Nelson <traveltimn@gmail.com>
2022-01-22 17:18:31 +0000

    Remove profile update functionality from accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -1,30 +1,44 @@
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, render  # , redirect, reverse
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile
-from .forms import UserProfileForm
+# from .models import Profile
+# from .forms import UserUpdateForm
+
+
+# @login_required
+# def profile(request):
+#     """ Display the user's profile. """
+#     user = get_object_or_404(User, username=request.user)
+
+#     if request.method == "POST":
+#         form = UserUpdateForm(request.POST, instance=user)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Profile successfully updated")
+#         else:
+#             messages.error(
+#                 request,
+#                 ("Update failed. Please ensure the form is valid."))
+#     else:
+#         form = UserUpdateForm(instance=user)
+
+#     template = "accounts/profile.html"
+#     context = {
+#         "form": form,
+#     }
+
+#     return render(request, template, context)
 
 
 @login_required
-def profile(request, username):
+def profile(request):
     """ Display the user's profile. """
-    user_profile = get_object_or_404(UserProfile, user=request.user)
+    # NO USE OF 'User' model, form simply using 'request.user' default
 
-    if request.method == "POST":
-        form = UserProfileForm(request.POST, instance=user_profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Profile successfully updated")
-        else:
-            messages.error(
-                request,
-                ("Update failed. Please ensure the form is valid."))
-    else:
-        form = UserProfileForm(instance=user_profile)
+    # user = get_object_or_404(User, username=request.user)
 
     template = "accounts/profile.html"
-    context = {
-        "form": form,
-    }
+    context = {}
 
     return render(request, template, context)
