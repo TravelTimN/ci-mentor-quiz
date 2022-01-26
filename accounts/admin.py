acb267ba3685551ba3d5816acc88c1d3851eff01
Tim Nelson <traveltimn@gmail.com>
2022-01-26 14:30:32 +0000

    Update Admin dashboard for Profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,6 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "taken_quiz")  # table view
-    list_filter = ("taken_quiz",)  # sidebar filter
-    search_fields = ["user", "taken_quiz"]  # search box
+    list_display = ("user", "taken_quiz", "mentor_type")  # table view
+    list_filter = ("taken_quiz", "mentor_type",)  # sidebar filter
+    search_fields = ["user", "taken_quiz", "mentor_type"]  # search box
