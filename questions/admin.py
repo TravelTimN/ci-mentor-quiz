7c2407eed513777ae827549c6b85e8ea2a1d1a09
Tim Nelson <traveltimn@gmail.com>
2022-01-27 21:50:05 +0000

    Update Question admin dashboard

--- questions/admin.py
+++ questions/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ("question", "module", "subject", "type")  # table view
+    list_filter = ("type", "subject", "module",)  # sidebar filter
+    search_fields = ["question", "module", "subject", "type"]  # search box
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("question", "choice", "correct_answer")  # table view
+
+
+admin.site.register(Question, QuestionAdmin)
