70e2a0bfb53b289eea34bbcf887c08235f3c069d
Tim Nelson <traveltimn@gmail.com>
2022-01-27 21:48:16 +0000

    Add Quiz model schema

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+
+class Quiz(models.Model):
+    # quiz types available
+    QUIZ_TYPE = [
+        ("current_mentor", "Quiz for Mentors"),
+        ("potential_mentor", "Quiz for Potential Mentors"),
+    ]
+    name = models.CharField(choices=QUIZ_TYPE, max_length=20)
+    max_count = models.IntegerField(null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_questions(self):
+        return self.question_set.all()
+
+    class Meta:
+        verbose_name_plural = "Quizzes"
