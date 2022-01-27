97af02925aea914ede2eba43a46518b357feadb4
Tim Nelson <traveltimn@gmail.com>
2022-01-27 21:49:37 +0000

    Update Question model, add Choice model

--- questions/models.py
+++ questions/models.py
@@ -1,45 +1,48 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-class Subject(models.Model):
-    # course subject (HTML, JS, Flask, Django, etc.)
-    name = models.CharField(max_length=100, blank=False)
-    friendly_name = models.CharField(max_length=100, blank=False)
-
-    def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
-        return self.friendly_name
+from quizzes.models import Quiz
 
 
 class Module(models.Model):
     # LMS module selection
     name = models.CharField(max_length=100, blank=False)
-    friendly_name = models.CharField(max_length=100, blank=False)
 
     def __str__(self):
         return self.name
 
-    def get_friendly_name(self):
-        return self.friendly_name
+
+class Subject(models.Model):
+    # course subject (HTML, JS, Flask, Django, etc.)
+    name = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Question(models.Model):
-    # question bank
-    question = models.CharField(max_length=100, blank=False)
-    opt1 = models.CharField(max_length=50, blank=False)
-    opt2 = models.CharField(max_length=50, blank=False)
-    opt3 = models.CharField(max_length=50, blank=True)
-    opt4 = models.CharField(max_length=50, blank=True)
-    opt5 = models.CharField(max_length=50, blank=True)
-    answer = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)])
-    subject = models.ForeignKey(
-        "Subject", null=True, blank=True, on_delete=models.SET_NULL)
-    module = models.ForeignKey(
-        "Module", null=True, blank=True, on_delete=models.SET_NULL)
+    # question types (multiple choice, true/false, short-answer)
+    QUESTION_TYPE = [
+        ("multi", "Multiple Choice"),
+        ("bool", "True or False"),
+        ("short", "Short Answer"),
+    ]
+    question = models.CharField(max_length=200, blank=False)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    type = models.CharField(choices=QUESTION_TYPE, max_length=10)
 
     def __str__(self):
         return f"{self.pk}: {self.question}"
+
+    def get_choices(self):
+        return self.answer_set.all()
+
+
+class Choice(models.Model):
+    choice = models.CharField(max_length=100, null=True)
+    correct_answer = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.choice
