db7157c8cea1ecde4226371f1d83f21f6be222c7
Tim Nelson <traveltimn@gmail.com>
2022-01-31 17:35:20 +0000

    Add starter functions to generate quiz templates

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,1 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.models import User
+from .models import Quiz
+
+
+def quiz_info(request):
+    user = get_object_or_404(User, username=request.user)
+    quizzes = Quiz.objects.all()
+    return render(request, "quizzes/info.html", {"user": user, "quizzes": quizzes})
+
+
+def take_quiz(request, pk):
+    quiz = Quiz.objects.get(pk=pk)
+    return render(request, "quizzes/quiz.html", {"quiz": quiz})
