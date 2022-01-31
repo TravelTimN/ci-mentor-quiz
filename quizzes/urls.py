92efe68ebc39453ca0dcf512cf7ebe6089e5038c
Tim Nelson <traveltimn@gmail.com>
2022-01-31 22:42:09 +0000

    Add quiz_data url to patterns

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.quiz_info, name="quiz"),
     path("<int:pk>/", views.take_quiz, name="take_quiz"),
+    path("<int:pk>/data", views.quiz_data, name="quiz_data"),
 ]
