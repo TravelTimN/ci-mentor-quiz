2c9e3d901a2a217d58741bb3d5d0445d63c0be8b
Tim Nelson <traveltimn@gmail.com>
2022-02-01 17:24:29 +0000

    Add fetch POST request for sending user's quiz to the DB

--- quizzes/static/js/quiz.js
+++ quizzes/static/js/quiz.js
@@ -4,7 +4,7 @@ const dataUrl = window.location.href;
 const quizSubmitBtn = document.getElementById("quiz-submit-btn");
 let quizContainer = document.getElementById("quiz-container");
 
-fetch(`${dataUrl}data`)
+fetch(`${dataUrl}data/`)
     .then(response => response.json())
     .then(data => {
         // console.log(data);
@@ -19,39 +19,123 @@ fetch(`${dataUrl}data`)
                     <p>${val.question}</p>
                 `;
                 val.choices.forEach(choice => {
-                    quizContainer.innerHTML += `<p>`;
                     switch (val.type) {
                         case "radio":
                             quizContainer.innerHTML += `
-                                <input type="radio" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="" value="${choice}">
+                                <p>
+                                <input type="radio" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="choice" value="${choice}" required>
                                 <label for="${counter}-${val.type}" class="">${choice}</label>
+                                </p>
                             `;
                             break;
                         case "checkbox":
                             quizContainer.innerHTML += `
-                                <input type="checkbox" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="" value="${choice}">
+                                <p>
+                                <input type="checkbox" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="choice" value="${choice}">
                                 <label for="${counter}-${val.type}" class=""> - ${choice}</label>
+                                </p>
                             `;
                             break;
                         case "textarea":
-                            quizContainer.innerHTML += `<textarea id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class=""></textarea>`;
+                            quizContainer.innerHTML += `
+                                <p>
+                                <textarea id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="choice" required></textarea>
+                                </p>
+                            `;
                             break;
                         case "number":
-                            quizContainer.innerHTML += `<input type="number" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="">`;
+                            quizContainer.innerHTML += `
+                                <p>
+                                <input type="number" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="choice" required>
+                                </p>
+                            `;
                             break;
                         case "url":
-                            quizContainer.innerHTML += `<input type="url" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="">`;
+                            quizContainer.innerHTML += `
+                                <p>
+                                <input type="url" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="choice" required>
+                                </p>
+                            `;
                             break;
                         default:
-                            quizContainer.innerHTML += `<input type="text" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="">`;
+                            quizContainer.innerHTML += `
+                                <p>
+                                <input type="text" id="${counter}-${val.type}" name="${val.question.replace(/[^a-zA-Z]/g, '')}" class="choice" required>
+                                </p>
+                            `;
                             break;
                     }
-                    quizContainer.innerHTML += `</p>`;
                     counter++;
                 });
             });
         });
     })
-    .catch(function (error) {
+    .catch(error => {
         console.log(error);
     });
+
+const quizForm = document.getElementById("quiz-form");
+const csrfToken = document.getElementsByName("csrfmiddlewaretoken");
+
+function saveQuizResults() {
+    const userAnswers = {};
+    const choices = document.querySelectorAll(".choice");
+    // userAnswers["csrfmiddlewaretoken"] = csrfToken[0].value; // TODO: remove this - no longer needed???
+    choices.forEach(choice => {
+        switch (choice.type) {
+            case "radio":
+            case "checkbox":
+                if (choice.checked) {
+                    // store 'name' key as the value of the question_type from user
+                    userAnswers[choice.name] = choice.value;
+                } else {
+                    if (!userAnswers[choice.name]) {
+                        // if 'name' key not available, this question hasn't been answered
+                        userAnswers[choice.name] = null;
+                    }
+                }
+                break;
+            case "textarea":
+            case "number":
+            case "url":
+            case "text":
+                if (choice.value) {
+                    // store 'name' key as the value of the question_type from user
+                    userAnswers[choice.name] = choice.value;
+                } else {
+                    if (!userAnswers[choice.name]) {
+                        // if 'name' key not available, this question hasn't been answered
+                        userAnswers[choice.name] = null;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    });
+
+    // https://docs.djangoproject.com/en/3.2/ref/csrf/#setting-the-token-on-the-ajax-request
+    const request = new Request(
+        `${dataUrl}save/`,
+        {headers: {"X-CSRFToken": csrfToken[0].value}}
+    );
+
+    fetch(request, {
+        method: "POST",
+        body: JSON.stringify(userAnswers),
+        mode: "same-origin",
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            console.log(userAnswers);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+quizForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    saveQuizResults();
+});
