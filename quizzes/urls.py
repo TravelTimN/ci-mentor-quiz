e212a9c85db370d30348132ba51d896d24753839
Tim Nelson <traveltimn@gmail.com>
2022-02-01 17:21:59 +0000

    Update quizzes urls to include save_quiz view

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.quiz_info, name="quiz"),
     path("<int:pk>/", views.take_quiz, name="take_quiz"),
-    path("<int:pk>/data", views.quiz_data, name="quiz_data"),
+    path("<int:pk>/save/", views.save_quiz_results, name="save_quiz"),
+    path("<int:pk>/data/", views.quiz_data, name="quiz_data"),
 ]
