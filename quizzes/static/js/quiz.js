caddd6b7f469f1c26fbc309f9ef2be907ec86ad2
Tim Nelson <traveltimn@gmail.com>
2022-02-02 20:57:38 +0000

    Fix Issue#2, looping user's answers and appending to array

--- quizzes/static/js/quiz.js
+++ quizzes/static/js/quiz.js
@@ -1,8 +1,28 @@
 /* jshint esversion: 8 */
 
 const dataUrl = window.location.href;
+const quizStartBtn = document.getElementById("quiz-start-btn");
 const quizSubmitBtn = document.getElementById("quiz-submit-btn");
+const quizForm = document.getElementById("quiz-form");
+const csrfToken = document.getElementsByName("csrfmiddlewaretoken");
 let quizContainer = document.getElementById("quiz-container");
+let time = 0;
+let timeInterval;
+
+
+quizStartBtn.addEventListener("click", function() {
+    this.classList.add("hide", "full");
+    quizForm.classList.remove("hide");
+    
+    timeInterval = setInterval(setTime, 1000);
+});
+
+
+function setTime() {
+  ++time;
+  console.log(time);
+}
+
 
 fetch(`${dataUrl}data/`)
     .then(response => response.json())
@@ -75,23 +95,40 @@ fetch(`${dataUrl}data/`)
         console.log(error);
     });
 
-const quizForm = document.getElementById("quiz-form");
-const csrfToken = document.getElementsByName("csrfmiddlewaretoken");
 
 function saveQuizResults() {
     const userAnswers = {};
     const choices = document.querySelectorAll(".choice");
     // userAnswers["csrfmiddlewaretoken"] = csrfToken[0].value; // TODO: remove this - no longer needed???
     choices.forEach(choice => {
+        // storing "choice.name" value to userAnswers dict
+        // must cast as Array() for the fetch data to match on DB
         switch (choice.type) {
             case "radio":
-            case "checkbox":
                 if (choice.checked) {
-                    // store 'name' key as the value of the question_type from user
-                    userAnswers[choice.name] = choice.value;
+                    // if radio is checked
+                    userAnswers[choice.name] = Array(choice.value);
                 } else {
                     if (!userAnswers[choice.name]) {
-                        // if 'name' key not available, this question hasn't been answered
+                        // field not checked, user left if blank
+                        userAnswers[choice.name] = null;
+                    }
+                }
+                break;
+            case "checkbox":
+            // checkbox needs to be an array, to handle multiple values
+                if (choice.checked) {
+                    // if checkbox is checked
+                    if (!userAnswers[choice.name]) {
+                        // if no existing options are checked
+                        userAnswers[choice.name] = Array(choice.value);
+                    } else {
+                        // additional options are also checked
+                        userAnswers[choice.name].push(choice.value);
+                    }
+                } else {
+                    if (!userAnswers[choice.name]) {
+                        // field not checked, user left if blank
                         userAnswers[choice.name] = null;
                     }
                 }
@@ -101,19 +138,22 @@ function saveQuizResults() {
             case "url":
             case "text":
                 if (choice.value) {
-                    // store 'name' key as the value of the question_type from user
-                    userAnswers[choice.name] = choice.value;
+                    // grab the user's value
+                    userAnswers[choice.name] = Array(choice.value);
                 } else {
                     if (!userAnswers[choice.name]) {
-                        // if 'name' key not available, this question hasn't been answered
+                        // field not checked, user left if blank
                         userAnswers[choice.name] = null;
                     }
                 }
                 break;
             default:
+                // TODO: what needs to be the default here?
                 break;
         }
     });
+    // add the total duration of 'time'
+    userAnswers["duration"] = time;
 
     // https://docs.djangoproject.com/en/3.2/ref/csrf/#setting-the-token-on-the-ajax-request
     const request = new Request(
@@ -128,8 +168,7 @@ function saveQuizResults() {
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            console.log(userAnswers);
+            console.log(data); // JsonResponse({}) from views.py
         })
         .catch(error => {
             console.log(error);
@@ -138,5 +177,8 @@ function saveQuizResults() {
 
 quizForm.addEventListener("submit", function(e) {
     e.preventDefault();
+    clearInterval(timeInterval);
+    quizForm.classList.add("hide", "full");
+    document.getElementById("thank-you").classList.remove("hide", "full");
     saveQuizResults();
 });
