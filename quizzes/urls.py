b04fe27acaeee0814cac06645cc2128b5ce27838
Tim Nelson <traveltimn@gmail.com>
2022-02-03 23:19:37 +0000

    Add quiz submit url, and test removal of JS method

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.quiz_info, name="quiz"),
     path("<int:pk>/", views.take_quiz, name="take_quiz"),
-    path("<int:pk>/save/", views.save_quiz_results, name="save_quiz"),
-    path("<int:pk>/data/", views.quiz_data, name="quiz_data"),
+    path("<int:pk>/submit/", views.submit_quiz_results, name="submit_quiz"),
+    # path("<int:pk>/save/", views.save_quiz_results, name="save_quiz"),
+    # path("<int:pk>/data/", views.quiz_data, name="quiz_data"),
 ]
