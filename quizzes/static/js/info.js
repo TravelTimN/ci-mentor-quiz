64bad1f0a44ab9f39fd012157f88b877c039fd1b
Tim Nelson <traveltimn@gmail.com>
2022-02-09 20:50:08 +0000

    Add optional-chaining ?. to eventListener

--- quizzes/static/js/info.js
+++ quizzes/static/js/info.js
@@ -2,7 +2,7 @@
 
 const takeQuizBtn = document.getElementById("btn-take-quiz");
 
-takeQuizBtn.addEventListener("click", function() {
+takeQuizBtn?.addEventListener("click", function() {
     // take the user to the appropriate quiz page
     let currentPath = window.location.pathname;
     let quiz = this.dataset.quiz;
