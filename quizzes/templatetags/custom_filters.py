a869e2482c83d04cc70d649189c72fa648196102
Tim Nelson <traveltimn@gmail.com>
2022-02-10 22:02:02 +0000

    Rename and add more Django custom_filters

---
+++ quizzes/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+import random
+from django import template
+register = template.Library()
+
+
+@register.filter
+def shuffle(arg):
+    # https://stackoverflow.com/a/7162816
+    shuffled_list = list(arg)[:]
+    random.shuffle(shuffled_list)
+    return shuffled_list
+
+
+@register.filter
+def replace(string, replace="*|*"):
+    # https://stackoverflow.com/a/35477957
+    find, replace = replace.split("|")
+    return string.replace(find, replace)
