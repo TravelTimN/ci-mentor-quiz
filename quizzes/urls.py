8e737bcf5d88c89030e007c1c491d4f6d653c5ab
Tim Nelson <traveltimn@gmail.com>
2022-03-10 14:57:44 +0000

    Remove unused URLs for fetching the quiz

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path("", views.quiz, name="quiz"),
     path("<int:pk>/", views.take_quiz, name="take_quiz"),
     path("<int:pk>/submit/", views.submit_quiz_results, name="submit_quiz"),
-    # path("<int:pk>/save/", views.save_quiz_results, name="save_quiz"),
-    # path("<int:pk>/data/", views.quiz_data, name="quiz_data"),
 ]
