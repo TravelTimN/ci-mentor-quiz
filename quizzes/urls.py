e99510228719f37212fc882cb666bfb63f057db8
Tim Nelson <traveltimn@gmail.com>
2022-03-10 12:57:46 +0000

    Update quiz_info URL to simplify redirect to profile

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.quiz_info, name="quiz"),
+    path("", views.quiz, name="quiz"),
     path("<int:pk>/", views.take_quiz, name="take_quiz"),
     path("<int:pk>/submit/", views.submit_quiz_results, name="submit_quiz"),
     # path("<int:pk>/save/", views.save_quiz_results, name="save_quiz"),
