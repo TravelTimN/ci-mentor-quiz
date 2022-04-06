485ed587785e5c7e807e45e15da27a128a9d37e2
Tim Nelson <traveltimn@gmail.com>
2022-04-06 19:25:47 +0000

    Add SITE_NAME env-var to env_sample file

--- env_sample.py
+++ env_sample.py
@@ -2,4 +2,5 @@ import os
 
 os.environ.setdefault("DATABASE_URL", "your_postgres_database_url")
 os.environ.setdefault("SECRET_KEY", "your_super_secret_key")
+os.environ.setdefault("SITE_NAME", "your_deployed_url_site_name")
 os.environ.setdefault("DEBUG", "True")
