241f39a9c5b4df5bc30285f0c1e9fb5f36a7d2d7
Tim Nelson <traveltimn@gmail.com>
2022-04-06 19:13:29 +0000

    Update fake env_sample file with DATABASE_URL example

--- env_sample.py
+++ env_sample.py
@@ -1,4 +1,5 @@
 import os
 
+os.environ.setdefault("DATABASE_URL", "your_postgres_database_url")
 os.environ.setdefault("SECRET_KEY", "your_super_secret_key")
 os.environ.setdefault("DEBUG", "True")
