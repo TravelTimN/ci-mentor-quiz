fc774285b0773e015aa6c875fba899bbd7cad0a4
Tim Nelson <traveltimn@gmail.com>
2022-04-13 11:55:59 +0000

    Update Accounts admin.py list display fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,6 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "taken_quiz", "mentor_type")  # table view
+    list_display = ("display_name", "taken_quiz", "mentor_type",)  # table view
     list_filter = ("taken_quiz", "mentor_type",)  # sidebar filter
-    search_fields = ["user__username", "user__email",]  # search box
+    search_fields = ["user__username", "user__email", ]  # search box
