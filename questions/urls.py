229e8a26cc86c08b3f7abef5edd1ece6eb38c8df
Tim Nelson <traveltimn@gmail.com>
2022-05-12 21:37:20 +0000

    Include add and update urls for questions

--- questions/urls.py
+++ questions/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.questions, name="questions"),
+    path("add/", views.add_question, name="add_question"),
+    path("update/<int:id>", views.update_question, name="update_question"),
 ]
