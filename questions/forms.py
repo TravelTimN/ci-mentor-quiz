39022ca64750d55213203054e0b21dafff211ad9
Tim Nelson <traveltimn@gmail.com>
2022-05-18 17:33:41 +0000

    Update ChoiceForm widget, and fix required attributes on Question

--- questions/forms.py
+++ questions/forms.py
@@ -9,46 +9,47 @@ class QuestionForm(forms.ModelForm):
         Form to allow Admin to build a new Question on the DB.
     """
     question = forms.CharField(
-        required=True,
         widget=forms.TextInput(attrs={
+            "required": True,
             "class": "form-control",
             "placeholder": "Question"
         })
     )
     quiz = forms.ModelChoiceField(
-        required=True,
         queryset=Quiz.objects.all(),
         empty_label="Select Quiz",
         widget=forms.Select(attrs={
+            "required": True,
             "class": "form-control",
         }),
     )
     module = forms.ModelChoiceField(
-        required=True,
         queryset=Module.objects.all(),
         empty_label="Select Module",
         widget=forms.Select(attrs={
+            "required": True,
             "class": "form-control",
         }),
     )
     subject = forms.ModelChoiceField(
-        required=True,
         queryset=Subject.objects.all(),
         empty_label="Select Subject",
         widget=forms.Select(attrs={
+            "required": True,
             "class": "form-control",
         }),
     )
     type = forms.ChoiceField(
-        required=True,
         choices=Question.QUESTION_TYPE,
         widget=forms.Select(attrs={
+            "required": True,
             "class": "form-control",
         }),
     )
     optional_text = forms.CharField(
         required=False,
         widget=forms.Textarea(attrs={
+            "required": False,
             "class": "form-control",
             "placeholder": "Optional Helper Text",
             "rows": 2,
@@ -61,6 +62,12 @@ class QuestionForm(forms.ModelForm):
 
 
 class ChoiceForm(forms.ModelForm):
+    choice = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "placeholder": "Choice #1",
+        })
+    )
+
     class Meta:
         model = Choice
         fields = ("choice", "correct_answer",)
