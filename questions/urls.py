04599c035a148527dc5035fe56d3d16fa8a4699e
Tim Nelson <traveltimn@gmail.com>
2022-06-22 18:31:00 +0000

    Add delete_question to urls

--- questions/urls.py
+++ questions/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.questions, name="questions"),
     path("add/", views.add_question, name="add_question"),
     path("update/<int:id>", views.update_question, name="update_question"),
+    path("delete/<int:id>", views.delete_question, name="delete_question"),
 ]
