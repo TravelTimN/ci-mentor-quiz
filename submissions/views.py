befaba4d4f58511ca5246a98e3a3b8510fa3c538
Tim Nelson <traveltimn@gmail.com>
2022-06-29 16:44:24 +0000

    Build AJAX view for posting new quiz attempts by user

--- submissions/views.py
+++ submissions/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .models import Attempt
+
+
+@login_required
+def ajax_quiz_start(request):
+    """
+    User has clicked the 'Start Quiz' button.
+    AJAX call to submit new 'Attempt' model.
+    This is to see how often a user is revisiting the quiz.
+    Some potential mentors have submitted the entire quiz in 2mins.
+    Likely reviewed the questions first, then copy/pasted in the form.
+    """
+    user = get_object_or_404(User, username=request.user)
+    if request.is_ajax():
+        attempt = Attempt.objects.create(user=user)
+        return HttpResponse("AJAX Success")
+    else:
+        return redirect("profile")
