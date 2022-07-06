871e8b5b4b6f59c1460f1cbeed47c8e57103b2de
Tim Nelson <traveltimn@gmail.com>
2022-07-06 12:25:05 +0000

    Add default 'DEBUG' variable if none present

--- main/settings.py
+++ main/settings.py
@@ -6,7 +6,7 @@ if os.path.exists("env.py"):
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 host = os.environ.get("SITE_NAME")
