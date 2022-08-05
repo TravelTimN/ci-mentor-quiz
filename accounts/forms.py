9285ccfb9cdad1a2e57c8ad4cfa9f4e302d5668f
Tim Nelson <traveltimn@gmail.com>
2022-08-05 12:04:37 +0000

    Add django-country / country of residence to signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+from django_countries import countries
 from allauth.account.forms import LoginForm, SignupForm
 from .models import Profile
 
@@ -23,6 +25,9 @@ class CustomSignupForm(SignupForm):
             "placeholder": "Last Name"
         })
     )
+    resident_country = CountryField(
+        blank=False).formfield(
+            widget=forms.Select(attrs={"class": "form-control"}))
     mentor_type = forms.ChoiceField(
         required=True,
         choices=Profile.MENTOR_TYPE,
@@ -39,6 +44,12 @@ class CustomSignupForm(SignupForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields["resident_country"].choices = [
+            ["", "Country of Residence"]
+        ]
+        for country in countries:
+            self.fields["resident_country"].choices.append(country)
+
         # override class and placeholder attributes
         self.fields["email"].widget.attrs.update(
             {"class": "form-control",
@@ -56,9 +67,10 @@ class CustomSignupForm(SignupForm):
         # set first_name to be autofocs on page load
         self.fields["first_name"].widget.attrs["autofocus"] = True
 
-    # override the profile's "mentor_type" field on registration
+    # override the user's profile fields on registration
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
+        user.profile.resident_country = self.cleaned_data["resident_country"]
         user.profile.mentor_type = self.cleaned_data["mentor_type"]
         user.save()
         return user
