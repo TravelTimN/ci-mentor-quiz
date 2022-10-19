80422e1b1b94a3b3cfa1f094b6686ae16e161e15
Tim Nelson <traveltimn@gmail.com>
2022-10-19 20:48:14 +0000

    Update quizzes-urls for managing quizzes

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.quiz, name="quiz"),
+    path("manage/", views.quizzes, name="quizzes"),
+    path("add/", views.add_quiz, name="add_quiz"),
     path("<int:pk>/", views.take_quiz, name="take_quiz"),
     path("<int:pk>/submit/", views.submit_quiz_results, name="submit_quiz"),
 ]
