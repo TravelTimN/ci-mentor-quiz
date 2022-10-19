65a5c8ce98d88e7d62ca76588dc49cab84629ccd
Tim Nelson <traveltimn@gmail.com>
2022-10-19 20:40:08 +0000

    Update url path for quizzes

--- accounts/static/js/profile.js
+++ accounts/static/js/profile.js
@@ -5,5 +5,5 @@ const takeQuizBtn = document.getElementById("btn-take-quiz");
 takeQuizBtn?.addEventListener("click", function() {
     // take the user to the appropriate quiz page
     let quiz = this.dataset.quiz;
-    window.location.assign(`/quiz/${quiz}`);
+    window.location.assign(`/quizzes/${quiz}`);
 });
