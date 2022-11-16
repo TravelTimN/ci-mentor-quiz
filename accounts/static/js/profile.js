ecf8a5cddba9c3c2a1bd3e1cb7336ac235b3bda1
Tim Nelson <traveltimn@gmail.com>
2022-11-16 13:07:42 +0000

    Remove static JS file for profile no longer needed

--- accounts/static/js/profile.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion: 8 */
-
-const takeQuizBtn = document.getElementById("btn-take-quiz");
-
-takeQuizBtn?.addEventListener("click", function() {
-    // take the user to the appropriate quiz page
-    let quiz = this.dataset.quiz;
-    window.location.assign(`/quizzes/${quiz}`);
-});
